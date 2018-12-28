37471d8a0ae6f481c90b119bb0b0b01c2e3fdedf
Chris Carey <chris.carey@gmail.com>
2018-12-27 17:04:27 -0800

    Tweak font size

--- src/components/ServiceItems.jsx
+++ src/components/ServiceItems.jsx
@@ -46,8 +46,7 @@ class ServiceItems extends Component {
       <div className="ServiceItems">
 
         {this.props.serviceProblemsArray.length > 0 && filteredServiceProblemsArray.length === 0 && <div key={'hiddenSummary'} className="margin-top-10 color-green some-down-items">
-          {this.props.howManyServices} services{', '}
-          {howManyHidden} hidden{' '}
+          {howManyHidden} hidden service problems{' '}
           {this.props.howManyServiceWarning > 0 && <span className="hidden-label warning">{this.props.howManyServiceWarning} WARNING</span>}
           {this.props.howManyServiceUnknown > 0 && <span className="hidden-label unknown">{this.props.howManyServiceUnknown} UNKNOWN</span>}
           {this.props.howManyServiceCritical > 0 && <span className="hidden-label critical">{this.props.howManyServiceCritical} CRITICAL</span>}
