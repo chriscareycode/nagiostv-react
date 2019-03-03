e8096c539675520d6b2a7c41c611013f84b722f6
Chris Carey <chris.carey@gmail.com>
2019-03-02 19:27:28 -0800

    Properly checking for instanceof DOMException now for audio failure

--- src/helpers/audio.js
+++ src/helpers/audio.js
@@ -5,52 +5,57 @@
  * https://developers.google.com/web/updates/2017/09/autoplay-policy-changes
  */
 export function playSoundEffect(type, state, settings) {
-	
-	const audioCritical = settings.soundEffectCritical;
-	const audioWarning = settings.soundEffectWarning;
-	const audioOk = settings.soundEffectOk;
+  
+  const audioCritical = settings.soundEffectCritical;
+  const audioWarning = settings.soundEffectWarning;
+  const audioOk = settings.soundEffectOk;
 
-	//console.log('playSoundEffect', type, state, audioCritical, audioWarning, audioOk);
+  //console.log('playSoundEffect', type, state, audioCritical, audioWarning, audioOk);
 
-	let audio;
+  let audio;
 
-	switch(type+state) {
-		case 'hostdown':
-			audio = new Audio(audioCritical);
-			break;
-		case 'hostup':
-			audio = new Audio(audioOk);
-			break;
-		case 'servicecritical':
-			audio = new Audio(audioCritical);
-			break;
-		case 'servicewarning':
-			audio = new Audio(audioWarning);
-			break;
-		case 'serviceok':
-			audio = new Audio(audioOk);
-			break;
-		default:
-			break;
-	}
+  switch(type+state) {
+    case 'hostdown':
+      audio = new Audio(audioCritical);
+      break;
+    case 'hostup':
+      audio = new Audio(audioOk);
+      break;
+    case 'servicecritical':
+      audio = new Audio(audioCritical);
+      break;
+    case 'servicewarning':
+      audio = new Audio(audioWarning);
+      break;
+    case 'serviceok':
+      audio = new Audio(audioOk);
+      break;
+    default:
+      break;
+  }
 
-	if (audio) {
-		
-		const promise = audio.play();
-		promise.catch((err) => {
-			console.log('error', err);
-			if (err === 'DomException') {
-				console.log('Blocked by Chrome autoplay prevention. Touch the UI to enable sound');
-			}
-		});
-	}
+  if (audio) {
+    const promise = audio.play();
+    promise.catch((err) => {
+      if (err instanceof DOMException) {
+        console.log(err.message);
+        //console.log(err.code);
+        //console.log(err.name);
+        console.log('Blocked by Chrome autoplay prevention. Touch the UI to enable sound');
+      } else {
+        console.log('error');
+        console.log(err);
+        console.log(typeof err);
+      }
+    });
+  }
 }
 
 export function speakAudio(words) {
-	
-	//console.log('speakAudio', words);
+  
+  //console.log('speakAudio', words);
 
-	const msg = new SpeechSynthesisUtterance(words);
-	window.speechSynthesis.speak(msg);
-	
+  const msg = new SpeechSynthesisUtterance(words);
+  window.speechSynthesis.speak(msg);
+  
 }
