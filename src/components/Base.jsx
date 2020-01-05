97bba4769751ebfc585d730f5152ffe5112ec2a2
Chris Carey <chris.carey@gmail.com>
2020-01-05 10:46:34 -0800

    Bump to 0.4.9

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -33,8 +33,8 @@ class Base extends Component {
   state = {
     showSettings: false,
 
-    currentVersion: 39,
-    currentVersionString: '0.4.8',
+    currentVersion: 40,
+    currentVersionString: '0.4.9',
     latestVersion: 0,
     latestVersionString: '',
     lastVersionCheckTime: 0,
