31cc3e0db4e8de977ceac494d850f65413522889
Chris Carey <chris.carey@gmail.com>
2020-03-10 19:28:30 -0700

    CustomLogo widget

---
+++ src/components/widgets/CustomLogo.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './CustomLogo.css';
+
+class CustomLogo extends Component {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    //console.log('shouldComponentUpdate', nextProps, nextState);
+    if (nextProps.settings.customLogoEnabled !== this.props.settings.customLogoEnabled || nextProps.settings.customLogoUrl !== this.props.settings.customLogoUrl) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  render() {
+    return (
+      <div className="CustomLogo">
+        <img src={this.props.settings.customLogoUrl} />
+      </div>
+    );
+  }
+}
+
+export default CustomLogo;
