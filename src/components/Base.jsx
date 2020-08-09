8ccdd92ab5f61502c68faf6b4018134b8304e56b
Chris Carey <chris.carey@gmail.com>
2020-08-09 12:45:29 -0700

    Bump to 0.5.6

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -38,8 +38,8 @@ class Base extends Component {
   state = {
     currentPage: 'dashboard',
 
-    currentVersion: 48,
-    currentVersionString: '0.5.5',
+    currentVersion: 49,
+    currentVersionString: '0.5.6',
     latestVersion: 0,
     latestVersionString: '',
     lastVersionCheckTime: 0,
