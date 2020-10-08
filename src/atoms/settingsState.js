a9a3a191eca9b204fd0fb0f09bdcabe875665b35
Chris Carey <chris.carey@gmail.com>
2020-10-07 22:35:07 -0700

    getting started with recoil

---
+++ src/atoms/settingsState.js
@@ -0,0 +1,4 @@
+export const settingsState = atom({
+  key: 'settingsState',
+  default: {}
+});
---
+++ src/settingsLoader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SettingsLoader = () => {
+  return (
+    <div className="SettingsLoader">
+      SettingsLoader
+    </div>
+  );
+};
+
+export default SettingsLoader;
