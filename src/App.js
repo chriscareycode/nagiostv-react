4cf3d83de0c9fba30b533b3a550c28415ac06950
Chris Carey <chris.carey@gmail.com>
2020-12-04 16:25:33 -0800

    Drop recoil out until such time that I actually start experimenting with it. Then do it in a branch.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import { RecoilRoot } from 'recoil';
 import Base from './components/Base.jsx';
 
 const App = () => {
 
   return (
     <div className="App">
-      <RecoilRoot>
-        <Base />
-      </RecoilRoot>
+      <Base />
     </div>
   );
   
