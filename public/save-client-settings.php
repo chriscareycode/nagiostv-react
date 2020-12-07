42bc43288ab0f5058d589b008e6b2ba85bd826a6
Chris Carey <chris.carey@gmail.com>
2020-12-07 00:18:29 -0800

    save-client-settings.php fix indent

--- public/save-client-settings.php
+++ public/save-client-settings.php
@@ -1,18 +1,18 @@
 <?php
 
-        // capture JSON POST
-        $json = '';
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                $json = file_get_contents("php://input");
-        }
+  // capture JSON POST
+  $json = '';
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+          $json = file_get_contents("php://input");
+  }
 
-        // write JSON to file
-        $myfile = fopen("client-settings.json", "w") or die("Unable to open file!");
-        fwrite($myfile, $json);
-        fclose($myfile);
+  // write JSON to file
+  $myfile = fopen("client-settings.json", "w") or die("Unable to open file!");
+  fwrite($myfile, $json);
+  fclose($myfile);
 
-        header('Content-Type: application/json');
-		//echo json_encode($json);
-		echo $json;
+  header('Content-Type: application/json');
+  //echo json_encode($json);
+  echo $json;
 
 ?>
