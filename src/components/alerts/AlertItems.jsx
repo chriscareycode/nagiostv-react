8f573b4e7405e1a862fd1360c671802d38693198
Chris Carey <chris.carey@gmail.com>
2020-12-31 10:32:45 -0800

    Add license

--- src/components/alerts/AlertItems.jsx
+++ src/components/alerts/AlertItems.jsx
@@ -1,3 +1,21 @@
+/**
+ * NagiosTV https://nagiostv.com
+ * Copyright (C) 2008-2020 Chris Carey https://chriscarey.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import React, { Component } from 'react';
 import { translate } from '../../helpers/language';
 
--- src/helpers/audio.js
+++ src/helpers/audio.js
@@ -1,3 +1,21 @@
+/**
+ * NagiosTV https://nagiostv.com
+ * Copyright (C) 2008-2020 Chris Carey https://chriscarey.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // so this new Audio() function dumps a stacktrace when it cant find the audio file to play
 // we should add some extra test that the file exists, if possible
 /*
