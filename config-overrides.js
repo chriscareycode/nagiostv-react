3a71dc94ad7b80ef8ec8984348d25ca0dd59b09b
Chris Carey <chris.carey@gmail.com>
2021-10-31 10:55:09 -0700

    remove some stuff out of config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,5 @@
 /* config-overrides.js */
 
-// module.exports = function override(config, env) {
-//   // do stuff with the webpack config...
-//   return config
-// }
-
 // for react fast refresh
 const { override } = require('customize-cra')
 const { addReactRefresh } = require('customize-cra-react-refresh')
