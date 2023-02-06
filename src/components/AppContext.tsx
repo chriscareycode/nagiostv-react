50f1b5908fa81ea5715193736cc2a83c91454ee4
Chris Carey <chris@chriscarey.com>
2023-02-06 08:25:51 -0800

    More spaces to tabs

--- src/components/AppContext.tsx
+++ src/components/AppContext.tsx
@@ -3,22 +3,22 @@ import { createContext, useState } from "react";
 export const AppContext = createContext({});
 
 export function AppContextProvider({ children }) {
-  const [preset, setPreset] = useState("moveToLeftFromRight");
-  const [enterAnimation, setEnterAnimation] = useState("");
-  const [exitAnimation, setExitAnimation] = useState("");
+	const [preset, setPreset] = useState("moveToLeftFromRight");
+	const [enterAnimation, setEnterAnimation] = useState("");
+	const [exitAnimation, setExitAnimation] = useState("");
 
-  return (
-    <AppContext.Provider
-      value={{
-        preset,
-        enterAnimation,
-        exitAnimation,
-        setPreset,
-        setEnterAnimation,
-        setExitAnimation
-      }}
-    >
-      {children}
-    </AppContext.Provider>
-  );
+	return (
+		<AppContext.Provider
+			value={{
+				preset,
+				enterAnimation,
+				exitAnimation,
+				setPreset,
+				setEnterAnimation,
+				setExitAnimation
+			}}
+		>
+			{children}
+		</AppContext.Provider>
+	);
 }
