8dd4eb3264080170741f4c8ae09c12010025f8a0
Chris Carey <chris@chriscarey.com>
2024-04-16 08:38:28 -0700

    Convert AlertItem from class to functional component

--- src/components/alerts/AlertItem.tsx
+++ src/components/alerts/AlertItem.tsx
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { Component } from 'react';
 import { translate } from '../../helpers/language';
 import { momentFormatDateTime } from '../../helpers/moment';
 import { ifQuietFor } from '../../helpers/date-math';
@@ -42,25 +41,15 @@ interface AlertItemProps {
 	prevtime: number;
 }
 
-class AlertItem extends Component<AlertItemProps> {
+const AlertItem = (props: AlertItemProps) => {
 
-	shouldComponentUpdate(nextProps, nextState) {
-		// console.log('shouldComponentUpdate', nextProps, nextState);
-		// if (nextProps.prevtime !== this.props.prevtime) {
-		//   return true;
-		// } else {
-		//   return false;
-		// }
-		return false;
-	}
-
-	openNagiosHostPage = () => {
-		const isDemoMode = this.props.isDemoMode;
+	const openNagiosHostPage = () => {
+		const isDemoMode = props.isDemoMode;
 		if (isDemoMode) {
 			return;
 		}
 
-		const { e } = this.props;
+		const { e } = props;
 		let hostName;
 		if (e.object_type === 1) {
 			hostName = e.name;
@@ -68,19 +57,19 @@ class AlertItem extends Component<AlertItemProps> {
 		if (e.object_type === 2) {
 			hostName = e.host_name;
 		}
-		const baseUrl = this.props.settings.baseUrl;
+		const baseUrl = props.settings.baseUrl;
 		const url = encodeURI(`${baseUrl}extinfo.cgi?type=1&host=${hostName}`);
 		const win = window.open(url, '_blank');
 		win?.focus();
 	}
 
-	openNagiosServicePage = () => {
-		const isDemoMode = this.props.isDemoMode;
+	const openNagiosServicePage = () => {
+		const isDemoMode = props.isDemoMode;
 		if (isDemoMode) {
 			return;
 		}
 
-		const { e } = this.props;
+		const { e } = props;
 		let hostName;
 		if (e.object_type === 1) {
 			hostName = e.name;
@@ -88,63 +77,61 @@ class AlertItem extends Component<AlertItemProps> {
 		if (e.object_type === 2) {
 			hostName = e.host_name;
 		}
-		const baseUrl = this.props.settings.baseUrl;
+		const baseUrl = props.settings.baseUrl;
 		const url = encodeURI(`${baseUrl}extinfo.cgi?type=2&host=${hostName}&service=${e.description}`);
 		const win = window.open(url, '_blank');
 		win?.focus();
 	}
 
-	render() {
+	const { language, locale, dateFormat } = props;
+	const howMuchTimeIsQuietTime = 10;
+	const { e, i } = props;
+	const isSoft = e.state_type === 2;
 
-		const { language, locale, dateFormat } = this.props;
-		const howMuchTimeIsQuietTime = 10;
-		const { e, i } = this.props;
-		const isSoft = e.state_type === 2;
+	return (
+		<div>
+			{/* show quiet for */}
+			{(i > 0) && ifQuietFor(e.timestamp, props.prevtime, howMuchTimeIsQuietTime) &&
+				<QuietFor
+					nowtime={e.timestamp}
+					prevtime={props.prevtime}
+					//showEmoji={this.props.showEmoji}
+					language={props.language}
+				/>
+			}
+			{/* show alert item */}
+			<div className={`AlertItem ${alertBorderClass(e.object_type, e.state)}`}>
+				<div className={'AlertItemRight'}>
+					{/*isSoft && <span className="softIcon color-white"><FontAwesomeIcon icon={faAdjust} /></span>*/}
+					{/* {1 === 2 && <span>({e.state_type})</span>} */}
+					<span className={`uppercase alert-item-state-type-${e.state_type}`}>{translate(nagiosAlertStateType(e.state_type), language)}</span>
+					{' '}
+					{/* {1 === 2 && <span>({e.state})</span>} */}
+					{/* {1 === 2 && <span>({e.object_type})</span>} */}
+					<span className={`uppercase ${alertTextClass(e.object_type, e.state)}`}>{translate(nagiosAlertState(e.state), language)}{' '}</span>
 
-		return (
-			<div>
-				{/* show quiet for */}
-				{(i > 0) && ifQuietFor(e.timestamp, this.props.prevtime, howMuchTimeIsQuietTime) &&
-					<QuietFor
-						nowtime={e.timestamp}
-						prevtime={this.props.prevtime}
-						//showEmoji={this.props.showEmoji}
-						language={this.props.language}
-					/>
-				}
-				{/* show alert item */}
-				<div className={`AlertItem ${alertBorderClass(e.object_type, e.state)}`}>
-					<div className={'AlertItemRight'}>
-						{/*isSoft && <span className="softIcon color-white"><FontAwesomeIcon icon={faAdjust} /></span>*/}
-						{/* {1 === 2 && <span>({e.state_type})</span>} */}
-						<span className={`uppercase alert-item-state-type-${e.state_type}`}>{translate(nagiosAlertStateType(e.state_type), language)}</span>
-						{' '}
-						{/* {1 === 2 && <span>({e.state})</span>} */}
-						{/* {1 === 2 && <span>({e.object_type})</span>} */}
-						<span className={`uppercase ${alertTextClass(e.object_type, e.state)}`}>{translate(nagiosAlertState(e.state), language)}{' '}</span>
-
-						<div className="alert-item-right-date align-right">{momentFormatDateTime(e.timestamp, locale, dateFormat)}</div>
-
-					</div>
-					<span style={{ textAlign: 'left' }}>
-
-						<div style={{ marginTop: '2px' }}>
-							{/* host */}
-							{e.object_type === 1 && <span className="alert-item-host-name alert-item-clickable" onClick={this.openNagiosHostPage}>{e.name}</span>}
-							{/* service */}
-							{e.object_type === 2 && <span className="alert-item-host-name alert-item-clickable" onClick={this.openNagiosHostPage}>{e.host_name}</span>}
-							{' '}
-							<span className={alertTextClass(e.object_type, e.state)}>
-								{e.object_type === 2 && <span className="alert-item-description alert-item-clickable" onClick={this.openNagiosServicePage}>{e.description}</span>}
-								{e.plugin_output}
-							</span>
-						</div>
-					</span>
+					<div className="alert-item-right-date align-right">{momentFormatDateTime(e.timestamp, locale, dateFormat)}</div>
 
 				</div>
+				<span style={{ textAlign: 'left' }}>
+
+					<div style={{ marginTop: '2px' }}>
+						{/* host */}
+						{e.object_type === 1 && <span className="alert-item-host-name alert-item-clickable" onClick={openNagiosHostPage}>{e.name}</span>}
+						{/* service */}
+						{e.object_type === 2 && <span className="alert-item-host-name alert-item-clickable" onClick={openNagiosHostPage}>{e.host_name}</span>}
+						{' '}
+						<span className={alertTextClass(e.object_type, e.state)}>
+							{e.object_type === 2 && <span className="alert-item-description alert-item-clickable" onClick={openNagiosServicePage}>{e.description}</span>}
+							{e.plugin_output}
+						</span>
+					</div>
+				</span>
+
 			</div>
-		);
-	}
+		</div>
+	);
+	
 }
 
 export default AlertItem;
