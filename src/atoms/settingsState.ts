e8e30595554e6252da7addc9629499ee6d66286c
Chris Carey <chris@chriscarey.com>
2025-12-31 11:37:01 -0800

    Bump version to 0.9.7

--- src/atoms/settingsState.ts
+++ src/atoms/settingsState.ts
@@ -26,8 +26,8 @@ const useFakeSampleData = urlParams.get('fakedata') === 'true' || urlParams.get(
 
 const bigStateInitial: BigState = {
 
-	currentVersion: 84, // This gets incremented with each new release (manually)
-	currentVersionString: '0.9.6', // This gets incremented with each new release (manually)
+	currentVersion: 85, // This gets incremented with each new release (manually)
+	currentVersionString: '0.9.7', // This gets incremented with each new release (manually)
 
 	latestVersion: 0,
 	latestVersionString: '',
