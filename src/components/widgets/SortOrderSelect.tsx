b21e1fb95a2a50af25f9a95a7ea13afdfa9cf965
Chris Carey <chris@chriscarey.com>
2026-01-04 23:32:37 -0800

    add SortOrderSelect

---
+++ src/components/widgets/SortOrderSelect.tsx
@@ -0,0 +1,42 @@
+/**
+ * NagiosTV https://nagiostv.com
+ * Copyright (C) 2008-2025 Chris Carey https://chriscarey.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { ChangeEvent } from 'react';
+import { translate } from '../../helpers/language';
+
+export type SortOrder = 'newest' | 'oldest' | 'az' | 'za';
+
+interface SortOrderSelectProps {
+	value: string;
+	varName: string;
+	language: string;
+	onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+}
+
+const SortOrderSelect = ({ value, varName, language, onChange }: SortOrderSelectProps) => {
+	return (
+		<select value={value} data-varname={varName} onChange={onChange}>
+			<option value="newest">{translate('newest first', language)}</option>
+			<option value="oldest">{translate('oldest first', language)}</option>
+			<option value="az">{translate('A-Z', language)}</option>
+			<option value="za">{translate('Z-A', language)}</option>
+		</select>
+	);
+};
+
+export default SortOrderSelect;
